53b0a6fe6a65e9ca5a84eed057a893079653908c
⎯᪵⎯꯭̽🚬᪵᪳ ⃪꯭𝐁ʌ‌꯭ᴅ꯭𝐍꯭ᴀ‌꯭ᴍ꯭ ꯭🖤᪵᪳᪳𝆺꯭𝅥⎯꯭̽⟶᯦꯭ <rejemtahir@gmail.com>
2026-04-20 09:55:46 +0530

    Update import statement for VideosSearch module

--- AarumiMusic/platforms/Youtube.py
+++ AarumiMusic/platforms/Youtube.py
@@ -18,7 +18,7 @@ from AarumiMusic import LOGGER
 from AarumiMusic.utils.database import is_on_off
 from AarumiMusic.utils.formatters import time_to_seconds
 from config import YT_API_KEY, YTPROXY_URL as YTPROXY
-from py_yt import VideosSearch
+    from youtubesearchpython.__future__ import VideosSearch
 logger = LOGGER(__name__)
 
 def cookie_txt_file():
